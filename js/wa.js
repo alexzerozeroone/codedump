3c17d6f425bd5aae78dd3a4ca37a7b23c20bc52b
alex <github.dev.rand@gmail.com>
2021-09-14 19:22:01 +0300

    uhhh tabs and config and other cool stuff

--- js/wa.js
+++ js/wa.js
@@ -1,28 +1,56 @@
 /*
-    weird algorithm that javascript takes time to load
-    highest runtime is precise[0]
+    weird algorithm™️
 */
 
-estimations = [Math.random() * 3, Math.random() * 2]
-precise = [1.0067988845291673, 1.1654900800172863, 1.1804955081257766, 0.8884260601220697, 2]
-// test_number = random(estimations)
-// test_number = random(precise)
-// test_number = precise[0]
+/* not recommended to edit */
+    estimations = [Math.random() * 3, Math.random() * Math.random() * 1];
+/* *********************** */
 
-// turn off on slow devices / websites??
-logging = false
-// turn off to not log the result at all
-full_logging = true
-// log finder result while not being the hit number
-log_anyways = false
-// return after hit_number is found
-cancel = true
-// how many times the loop shoudl try to find a hit number
-tries = 1500
-// module to use for randomization
-random_module = estimations
-// hit number to go over
-hit_number = 10000
+/* ******************************************* */
+/* ******************************************* */
+
+    /* MODULARS */
+
+        // module to use for randomization
+        random_module = estimations;
+
+        // use single module
+        not_random = false;
+        single_module = estimations[estimations.length - 1];
+
+    /* MODULARS */
+
+/* ******************************************* */
+/* ******************************************* */
+
+    /* CONFIGURATION */
+
+        // hit number to go over
+        hit_number = 15000;
+
+        // how many times the loop shoudl try to find a hit number
+        tries = 9000;
+
+        // return after hit_number is found
+        cancel = true;
+
+    /* CONFIGURATION */
+
+/* ******************************************* */
+/* ******************************************* */
+
+    /* SCREEN */
+
+        // logging for f function
+        logging = false;
+
+        // clear screen
+        clear = true;
+
+    /* SCREEN */
+
+/* ******************************************* */
+/* ******************************************* */
 
 /*
     ****************************************************************
@@ -43,40 +71,39 @@ function random(array) {
 function f(nr) {
     if (nr / nr) {
         f(f(nr * nr));
-        logging ? console.log(nr) : null;
+        if (logging) console.log(nr);
         c++;
     }
 
     if (nr * nr / nr) {
         f(nr * nr);
-        logging ? console.log(nr) : null;
+        if (logging) console.log(nr);
         c++;
     }
 
     if (nr == Infinity) {
-        logging ? console.log("Reached infinity") : null;
+        if (logging) console.log("Reached Infinity");
         c++;
     }
 }
 
-function finder(array, tries, hit_number = 999999) {
+function finder(array, tries, hit_number) {
     var notfound = 0;
 
     for (var i = 0; i < tries; i++) {
-        test_number = random(array);
-        console.clear();
+        if (clear) console.clear();
+        if (random_module) { test_number = random(array); } else {  }
+
         var t0 = performance.now();
 
         f(test_number);
 
         var t1 = performance.now();
-        log_anyways ? console.log(`***\nCode took ${t1 - t0}ms to run, function ran ${c} times, original integer was ${test_number}\n***`) : null;
-        r = () => { return }
         if (c >= hit_number) {
-            console.log(`***!!!\nHit number found, information:`);
-            console.log(`\nCode took ${t1 - t0}ms to run, function ran ${c} times, original integer was ${test_number}\n`);
-            console.log("!!!***")
-            cancel ? r() : null;
+            console.log(`***\t\t\t\t\t\t\t\t\t\t\t\t\t\t***\n\tHit number found, information:`);
+            console.log(`\n\tCode took ${t1 - t0}ms to run, function ran ${c} times, original integer was ${test_number}\n`);
+            console.log(`***\t\t\t\t\t\t\t\t\t\t\t\t\t\t***`)
+            if (cancel) return;
         } else {
             notfound++;
         }
@@ -85,8 +112,8 @@ function finder(array, tries, hit_number = 999999) {
     }
 
     if (notfound == tries) {
-        console.clear();
-        console.log(`***\nNo result found\n***`)
+        if (clear) console.clear();
+        console.log(`***\t\t\t\t***\n\tNo result found\n***\t\t\t\t***`)
     }
 }
 
